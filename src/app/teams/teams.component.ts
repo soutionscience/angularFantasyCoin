f84fb280c7b7b1be7f4434c235dfa0ba80422504
soutionscience <marshnjagi@yahoo.com>
2018-09-14 06:05:38 +0300

    added web 3 successfully

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Generic } from '../shared/generic.model';
 import { ApiServiceService } from '../services/api-service.service';
+import { Web3Service } from '../services/web3.service';
 
 @Component({
   selector: 'app-teams',
@@ -10,11 +11,14 @@ import { ApiServiceService } from '../services/api-service.service';
 export class TeamsComponent implements OnInit {
   teams: Generic[]
   players: Generic[]
+  accounts: String [];
+  balance: String;
 
-  constructor(private apiService: ApiServiceService) { }
+  constructor(private apiService: ApiServiceService, private web3Service: Web3Service) { }
 
   ngOnInit (){
-    this.getTeams()
+    this.getTeams();
+    this.getAccounts();
     console.log("what is here? ", this.teams)
   }
 
@@ -24,4 +28,19 @@ export class TeamsComponent implements OnInit {
   selectTeam(p){
   this.apiService.getTeams('players', p).subscribe(resp=>{console.log("haha ",resp); this.players = resp})
   }
+  checkweb3(){
+    this.web3Service.checkAndInstatiateWeb3();
+  }
+  getAccounts(){
+    this.web3Service.getAccounts()
+    .subscribe(resp=>{
+      this.accounts=resp;
+      this.web3Service.getBalance(this.accounts[0])
+      .subscribe(resp=>{
+        this.balance = resp;
+        console.log(this.balance)
+      })
+    })
+  }
+
 }
